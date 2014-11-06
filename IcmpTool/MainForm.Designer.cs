6a557ee4e212f14b198cdf412338679b65d699de
Devendra Tewari <dkt@cesar.org.br>
2014-11-06 11:52:00 -0300

    Improve UI
    Improve UI of the application

--- IcmpTool/MainForm.Designer.cs
+++ IcmpTool/MainForm.Designer.cs
@@ -28,19 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.requestType = new System.Windows.Forms.ComboBox();
-            this.bind = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.inputText = new System.Windows.Forms.TextBox();
-            this.sourceIPAddress = new System.Windows.Forms.ComboBox();
             this.sendButton = new System.Windows.Forms.Button();
             this.destinationIPAddress = new System.Windows.Forms.TextBox();
+            this.bind = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.sourceIPAddress = new System.Windows.Forms.ComboBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.clearButton = new System.Windows.Forms.Button();
             this.outputText = new System.Windows.Forms.TextBox();
@@ -52,145 +52,135 @@
             // 
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.requestType);
-            this.panel1.Controls.Add(this.bind);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.inputText);
-            this.panel1.Controls.Add(this.sourceIPAddress);
             this.panel1.Controls.Add(this.sendButton);
             this.panel1.Controls.Add(this.destinationIPAddress);
-            this.panel1.Location = new System.Drawing.Point(2, 1);
+            this.panel1.Location = new System.Drawing.Point(386, 1);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(588, 212);
+            this.panel1.Size = new System.Drawing.Size(398, 439);
             this.panel1.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 178);
+            this.label2.Location = new System.Drawing.Point(9, 52);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(303, 13);
+            this.label2.Size = new System.Drawing.Size(311, 13);
             this.label2.TabIndex = 13;
-            this.label2.Text = "Text above is a hexadecimal string (e.g. 0xDE 0xAD or DE AD)";
+            this.label2.Text = "Message to send (hexadecimal string e.g. 0xDE 0xAD or DE AD)";
             // 
             // requestType
             // 
             this.requestType.FormattingEnabled = true;
             this.requestType.Items.AddRange(new object[] {
             "ICMP Echo Request (Ping)"});
-            this.requestType.Location = new System.Drawing.Point(11, 75);
+            this.requestType.Location = new System.Drawing.Point(5, 407);
             this.requestType.Name = "requestType";
-            this.requestType.Size = new System.Drawing.Size(174, 21);
-            this.requestType.TabIndex = 3;
+            this.requestType.Size = new System.Drawing.Size(385, 21);
+            this.requestType.TabIndex = 6;
             this.requestType.SelectedIndexChanged += new System.EventHandler(this.requestType_SelectedIndexChanged);
             this.requestType.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.requestType_KeyPress);
-            // 
-            // bind
-            // 
-            this.bind.Location = new System.Drawing.Point(135, 22);
-            this.bind.Name = "bind";
-            this.bind.Size = new System.Drawing.Size(75, 23);
-            this.bind.TabIndex = 2;
-            this.bind.Text = "Bind";
-            this.bind.UseVisualStyleBackColor = true;
-            this.bind.Click += new System.EventHandler(this.bind_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 59);
+            this.label3.Location = new System.Drawing.Point(3, 391);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.Size = new System.Drawing.Size(94, 13);
             this.label3.TabIndex = 8;
-            this.label3.Text = "Send";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(8, 8);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(95, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Interface (optional)";
+            this.label3.Text = "Useful Message(s)";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(188, 59);
+            this.label1.Location = new System.Drawing.Point(2, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.Size = new System.Drawing.Size(114, 13);
             this.label1.TabIndex = 6;
-            this.label1.Text = "To IP Address";
+            this.label1.Text = "Destination IP Address";
             // 
             // inputText
             // 
             this.inputText.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.inputText.Location = new System.Drawing.Point(11, 102);
+            this.inputText.Location = new System.Drawing.Point(5, 68);
             this.inputText.Multiline = true;
             this.inputText.Name = "inputText";
             this.inputText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.inputText.Size = new System.Drawing.Size(561, 70);
-            this.inputText.TabIndex = 5;
-            // 
-            // sourceIPAddress
-            // 
-            this.sourceIPAddress.FormattingEnabled = true;
-            this.sourceIPAddress.Location = new System.Drawing.Point(10, 24);
-            this.sourceIPAddress.Name = "sourceIPAddress";
-            this.sourceIPAddress.Size = new System.Drawing.Size(119, 21);
-            this.sourceIPAddress.TabIndex = 0;
+            this.inputText.Size = new System.Drawing.Size(385, 302);
+            this.inputText.TabIndex = 4;
             // 
             // sendButton
             // 
             this.sendButton.Enabled = false;
-            this.sendButton.Location = new System.Drawing.Point(497, 178);
+            this.sendButton.Location = new System.Drawing.Point(315, 376);
             this.sendButton.Name = "sendButton";
             this.sendButton.Size = new System.Drawing.Size(75, 23);
-            this.sendButton.TabIndex = 6;
+            this.sendButton.TabIndex = 5;
             this.sendButton.Text = "Send";
             this.sendButton.UseVisualStyleBackColor = true;
             this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
             // 
             // destinationIPAddress
             // 
-            this.destinationIPAddress.Location = new System.Drawing.Point(191, 75);
+            this.destinationIPAddress.Location = new System.Drawing.Point(5, 24);
             this.destinationIPAddress.Name = "destinationIPAddress";
             this.destinationIPAddress.Size = new System.Drawing.Size(120, 20);
-            this.destinationIPAddress.TabIndex = 4;
+            this.destinationIPAddress.TabIndex = 3;
+            // 
+            // bind
+            // 
+            this.bind.Location = new System.Drawing.Point(134, 23);
+            this.bind.Name = "bind";
+            this.bind.Size = new System.Drawing.Size(75, 23);
+            this.bind.TabIndex = 1;
+            this.bind.Text = "Bind";
+            this.bind.UseVisualStyleBackColor = true;
+            this.bind.Click += new System.EventHandler(this.bind_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Interface (optional)";
+            // 
+            // sourceIPAddress
+            // 
+            this.sourceIPAddress.FormattingEnabled = true;
+            this.sourceIPAddress.Location = new System.Drawing.Point(9, 24);
+            this.sourceIPAddress.Name = "sourceIPAddress";
+            this.sourceIPAddress.Size = new System.Drawing.Size(119, 21);
+            this.sourceIPAddress.TabIndex = 0;
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.label5);
             this.panel2.Controls.Add(this.label6);
             this.panel2.Controls.Add(this.clearButton);
+            this.panel2.Controls.Add(this.bind);
             this.panel2.Controls.Add(this.outputText);
-            this.panel2.Location = new System.Drawing.Point(2, 219);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.sourceIPAddress);
+            this.panel2.Location = new System.Drawing.Point(1, 1);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(588, 160);
-            this.panel2.TabIndex = 1;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(7, 139);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(165, 13);
-            this.label5.TabIndex = 14;
-            this.label5.Text = "The first 20 bytes is the IP header";
+            this.panel2.Size = new System.Drawing.Size(384, 439);
+            this.panel2.TabIndex = 0;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(8, 6);
+            this.label6.Location = new System.Drawing.Point(6, 52);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(127, 13);
+            this.label6.Size = new System.Drawing.Size(272, 13);
             this.label6.TabIndex = 9;
-            this.label6.Text = "ICMP messages received";
+            this.label6.Text = "ICMP messages received (first 20 bytes is the IP header)";
             // 
             // clearButton
             // 
-            this.clearButton.Location = new System.Drawing.Point(497, 134);
+            this.clearButton.Location = new System.Drawing.Point(301, 405);
             this.clearButton.Name = "clearButton";
             this.clearButton.Size = new System.Drawing.Size(75, 23);
             this.clearButton.TabIndex = 8;
@@ -201,21 +191,23 @@
             // outputText
             // 
             this.outputText.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputText.Location = new System.Drawing.Point(11, 22);
+            this.outputText.Location = new System.Drawing.Point(9, 68);
             this.outputText.Multiline = true;
             this.outputText.Name = "outputText";
+            this.outputText.ReadOnly = true;
             this.outputText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.outputText.Size = new System.Drawing.Size(562, 106);
-            this.outputText.TabIndex = 7;
+            this.outputText.Size = new System.Drawing.Size(367, 331);
+            this.outputText.TabIndex = 2;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 381);
+            this.ClientSize = new System.Drawing.Size(784, 441);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainForm";
@@ -247,7 +239,6 @@
         private System.Windows.Forms.Button bind;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox requestType;
-        private System.Windows.Forms.Label label5;
     }
 }
 
