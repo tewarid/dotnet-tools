8ae34061f32f729cc5ce476bd03537b78d86901e
Devendra Tewari <devendra@leme-2.local>
2015-05-28 08:34:20 -0300

    Allow minimizing

--- SerialTool/MainForm.Designer.cs
+++ SerialTool/MainForm.Designer.cs
@@ -372,7 +372,6 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Serial Tool";
