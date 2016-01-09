e00a22455781d79cf3091973f27a8d872273186c
Devendra Tewari <devendra.tewari@gmail.com>
2016-01-09 08:45:27 -0300

    Make form resizable

--- WebSocketSharpTool/MainForm.Designer.cs
+++ WebSocketSharpTool/MainForm.Designer.cs
@@ -35,30 +35,42 @@
             this.connect = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.proxyButton = new System.Windows.Forms.Button();
             this.setHeaders = new System.Windows.Forms.Button();
             this.outputText = new Common.ReceiveTextBox();
             this.closeButton = new System.Windows.Forms.Button();
             this.location = new System.Windows.Forms.TextBox();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.status = new System.Windows.Forms.ToolStripStatusLabel();
-            this.proxyButton = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.sendButton);
             this.panel1.Controls.Add(this.sendTextBox);
-            this.panel1.Location = new System.Drawing.Point(395, 2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(389, 434);
+            this.panel1.Padding = new System.Windows.Forms.Padding(0, 3, 0, 3);
+            this.panel1.Size = new System.Drawing.Size(389, 439);
             this.panel1.TabIndex = 0;
             // 
             // sendButton
             // 
-            this.sendButton.Location = new System.Drawing.Point(307, 397);
+            this.sendButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.sendButton.Location = new System.Drawing.Point(311, 403);
             this.sendButton.Name = "sendButton";
             this.sendButton.Size = new System.Drawing.Size(75, 30);
             this.sendButton.TabIndex = 7;
@@ -68,14 +80,16 @@
             // 
             // sendTextBox
             // 
-            this.sendTextBox.Location = new System.Drawing.Point(3, 4);
+            this.sendTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sendTextBox.Location = new System.Drawing.Point(0, 3);
             this.sendTextBox.Name = "sendTextBox";
-            this.sendTextBox.Size = new System.Drawing.Size(381, 425);
+            this.sendTextBox.Size = new System.Drawing.Size(389, 433);
             this.sendTextBox.TabIndex = 6;
             // 
             // connect
             // 
-            this.connect.Location = new System.Drawing.Point(230, 93);
+            this.connect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.connect.Location = new System.Drawing.Point(230, 90);
             this.connect.Name = "connect";
             this.connect.Size = new System.Drawing.Size(75, 23);
             this.connect.TabIndex = 3;
@@ -87,7 +101,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(3, 11);
+            this.label4.Location = new System.Drawing.Point(6, 3);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(283, 13);
             this.label4.TabIndex = 7;
@@ -95,21 +109,30 @@
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.proxyButton);
-            this.panel2.Controls.Add(this.setHeaders);
-            this.panel2.Controls.Add(this.outputText);
-            this.panel2.Controls.Add(this.closeButton);
-            this.panel2.Controls.Add(this.connect);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Controls.Add(this.location);
-            this.panel2.Location = new System.Drawing.Point(2, 2);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(389, 434);
+            this.panel2.Padding = new System.Windows.Forms.Padding(0, 3, 0, 3);
+            this.panel2.Size = new System.Drawing.Size(392, 439);
             this.panel2.TabIndex = 1;
+            // 
+            // proxyButton
+            // 
+            this.proxyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.proxyButton.Location = new System.Drawing.Point(109, 90);
+            this.proxyButton.Name = "proxyButton";
+            this.proxyButton.Size = new System.Drawing.Size(82, 23);
+            this.proxyButton.TabIndex = 2;
+            this.proxyButton.Text = "HTTP Proxy...";
+            this.proxyButton.UseVisualStyleBackColor = true;
+            this.proxyButton.Click += new System.EventHandler(this.proxyButton_Click);
             // 
             // setHeaders
             // 
-            this.setHeaders.Location = new System.Drawing.Point(6, 93);
+            this.setHeaders.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.setHeaders.Location = new System.Drawing.Point(6, 90);
             this.setHeaders.Name = "setHeaders";
             this.setHeaders.Size = new System.Drawing.Size(97, 23);
             this.setHeaders.TabIndex = 1;
@@ -119,15 +142,17 @@
             // 
             // outputText
             // 
-            this.outputText.Location = new System.Drawing.Point(6, 123);
+            this.outputText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputText.Location = new System.Drawing.Point(3, 3);
             this.outputText.Name = "outputText";
-            this.outputText.Size = new System.Drawing.Size(387, 311);
+            this.outputText.Size = new System.Drawing.Size(386, 306);
             this.outputText.TabIndex = 5;
             // 
             // closeButton
             // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.closeButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.closeButton.Location = new System.Drawing.Point(311, 93);
+            this.closeButton.Location = new System.Drawing.Point(311, 90);
             this.closeButton.Name = "closeButton";
             this.closeButton.Size = new System.Drawing.Size(75, 23);
             this.closeButton.TabIndex = 4;
@@ -137,10 +162,13 @@
             // 
             // location
             // 
-            this.location.Location = new System.Drawing.Point(6, 27);
+            this.location.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.location.Location = new System.Drawing.Point(6, 19);
             this.location.Multiline = true;
             this.location.Name = "location";
-            this.location.Size = new System.Drawing.Size(380, 60);
+            this.location.Size = new System.Drawing.Size(380, 65);
             this.location.TabIndex = 0;
             this.location.Text = "wss://echo.websocket.org?foo=bar";
             // 
@@ -159,15 +187,50 @@
             this.status.Name = "status";
             this.status.Size = new System.Drawing.Size(0, 17);
             // 
-            // proxyButton
+            // splitContainer1
             // 
-            this.proxyButton.Location = new System.Drawing.Point(109, 93);
-            this.proxyButton.Name = "proxyButton";
-            this.proxyButton.Size = new System.Drawing.Size(82, 23);
-            this.proxyButton.TabIndex = 2;
-            this.proxyButton.Text = "HTTP Proxy...";
-            this.proxyButton.UseVisualStyleBackColor = true;
-            this.proxyButton.Click += new System.EventHandler(this.proxyButton_Click);
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel2);
+            this.splitContainer1.Panel1MinSize = 370;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Panel2MinSize = 360;
+            this.splitContainer1.Size = new System.Drawing.Size(784, 439);
+            this.splitContainer1.SplitterDistance = 392;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 8;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.proxyButton);
+            this.panel3.Controls.Add(this.location);
+            this.panel3.Controls.Add(this.setHeaders);
+            this.panel3.Controls.Add(this.connect);
+            this.panel3.Controls.Add(this.closeButton);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(3);
+            this.panel3.Size = new System.Drawing.Size(392, 121);
+            this.panel3.TabIndex = 8;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.outputText);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 124);
+            this.panel4.Name = "panel4";
+            this.panel4.Padding = new System.Windows.Forms.Padding(3);
+            this.panel4.Size = new System.Drawing.Size(392, 312);
+            this.panel4.TabIndex = 9;
             // 
             // MainForm
             // 
@@ -176,13 +239,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.closeButton;
             this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "WebSocketSharp Tool";
@@ -190,9 +249,15 @@
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -213,6 +278,9 @@
         private Common.ReceiveTextBox outputText;
         private System.Windows.Forms.Button setHeaders;
         private System.Windows.Forms.Button proxyButton;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
