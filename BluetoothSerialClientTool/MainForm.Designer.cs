567701760da5b2853d9eba58e3948b3dc310712e
Devendra Tewari <Devendra Tewari>
2016-12-16 20:59:21 -0300

    Bluetooth serial client tool

---
+++ BluetoothSerialClientTool/MainForm.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace BluetoothSerialClientTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.outputText = new Common.ReceiveTextBox();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.deviceList = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.input = new Common.SendTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.status = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Size = new System.Drawing.Size(746, 436);
+            this.splitContainer1.SplitterDistance = 373;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.outputText);
+            this.panel2.Controls.Add(this.closeButton);
+            this.panel2.Controls.Add(this.refreshButton);
+            this.panel2.Controls.Add(this.deviceList);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.connectButton);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(373, 436);
+            this.panel2.TabIndex = 1;
+            // 
+            // outputText
+            // 
+            this.outputText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputText.Location = new System.Drawing.Point(6, 56);
+            this.outputText.MinimumSize = new System.Drawing.Size(355, 95);
+            this.outputText.Name = "outputText";
+            this.outputText.Size = new System.Drawing.Size(365, 377);
+            this.outputText.TabIndex = 5;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.Enabled = false;
+            this.closeButton.Location = new System.Drawing.Point(298, 25);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(64, 23);
+            this.closeButton.TabIndex = 4;
+            this.closeButton.Text = "&Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.refreshButton.Location = new System.Drawing.Point(158, 25);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(64, 23);
+            this.refreshButton.TabIndex = 1;
+            this.refreshButton.Text = "&Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // deviceList
+            // 
+            this.deviceList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.deviceList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.deviceList.FormattingEnabled = true;
+            this.deviceList.Location = new System.Drawing.Point(7, 27);
+            this.deviceList.Name = "deviceList";
+            this.deviceList.Size = new System.Drawing.Size(145, 21);
+            this.deviceList.TabIndex = 0;
+            this.deviceList.SelectedIndexChanged += new System.EventHandler(this.deviceList_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(4, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Paired Devices";
+            // 
+            // connectButton
+            // 
+            this.connectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.connectButton.Enabled = false;
+            this.connectButton.Location = new System.Drawing.Point(228, 25);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(64, 23);
+            this.connectButton.TabIndex = 3;
+            this.connectButton.Text = "C&onnect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.sendButton);
+            this.panel1.Controls.Add(this.input);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(369, 436);
+            this.panel1.TabIndex = 0;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.sendButton.Enabled = false;
+            this.sendButton.Location = new System.Drawing.Point(287, 401);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 30);
+            this.sendButton.TabIndex = 15;
+            this.sendButton.Text = "&Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // input
+            // 
+            this.input.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.input.Location = new System.Drawing.Point(3, 4);
+            this.input.MinimumSize = new System.Drawing.Size(280, 130);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(359, 426);
+            this.input.TabIndex = 12;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.status});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 436);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(746, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // status
+            // 
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 17);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 458);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bluetooth Serial Client Tool";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel2;
+        private Common.ReceiveTextBox outputText;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.ComboBox deviceList;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button sendButton;
+        private Common.SendTextBox input;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel status;
+    }
+}
+
---
+++ BluetoothSerialClientTool/MainForm.cs
@@ -0,0 +1,173 @@
+﻿using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BluetoothSerialClientTool
+{
+    public partial class MainForm : Form
+    {
+        private readonly Guid MyServiceUuid = new Guid("{00001101-0000-1000-8000-00805F9B34FB}");
+        // Android uses the well known SPP GUID 00001101-0000-1000-8000-00805F9B34FB
+
+        BluetoothClient client;
+        BluetoothDeviceInfo[] devices;
+        NetworkStream stream;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            client = new BluetoothClient();
+            devices = client.DiscoverDevices(10, true, true, false);
+
+            deviceList.Items.Clear();
+            foreach (BluetoothDeviceInfo device in devices)
+            {
+                deviceList.Items.Add(device.DeviceName);
+            }
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            if (deviceList.SelectedIndex >= devices.Length)
+                return;
+            try
+            {
+                client.Connect(devices[deviceList.SelectedIndex].DeviceAddress, MyServiceUuid);
+                stream = client.GetStream();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, this.Text);
+                return;
+            }
+            ReadAsync(stream);
+            deviceList.Enabled = false;
+            refreshButton.Enabled = false;
+            connectButton.Enabled = false;
+            closeButton.Enabled = true;
+            sendButton.Enabled = true;
+        }
+
+        private async void ReadAsync(NetworkStream stream)
+        {
+            byte[] buffer = new byte[100];
+            while (true)
+            {
+                try
+                {
+                    int length = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    Invoke(new MethodInvoker(delegate
+                    {
+                        outputText.Append(buffer, length);
+                    }));
+                }
+                catch
+                {
+                    if (this.Visible)
+                    { // we're not being closed
+                        Invoke(new MethodInvoker(delegate
+                        {
+                            Stop();
+                        }));
+                    }
+                    break;
+                }
+            }
+        }
+
+        private void Stop()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new MethodInvoker(delegate ()
+                {
+                    Stop();
+                }));
+                return;
+            }
+
+            if (stream != null)
+            {
+                stream.Close();
+                stream = null;
+            }
+            if (client != null)
+            {
+                client.Close();
+            }
+            deviceList.Enabled = true;
+            refreshButton.Enabled = true;
+            connectButton.Enabled = false;
+            closeButton.Enabled = false;
+            sendButton.Enabled = false;
+            Initialize();
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            Initialize();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            client.Close();
+        }
+
+        private void deviceList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            connectButton.Enabled = deviceList.SelectedIndex >= 0;
+        }
+
+        private async void SendAsync(byte[] buffer)
+        {
+            int startTickCount = 0;
+            int endTickCount = 0;
+
+            // this will run in a worker thread
+            await Task.Run(delegate
+            {
+                try
+                {
+                    startTickCount = Environment.TickCount;
+                    stream.Write(buffer, 0, buffer.Length);
+                    endTickCount = Environment.TickCount;
+                }
+                catch (Exception ex)
+                {
+                    Stop();
+                    MessageBox.Show(ex.Message);
+                }
+            });
+
+            // main thread gets resumed at this point
+            // so invoke not required
+            if (endTickCount != 0)
+                status.Text = String.Format("Sent {0} byte(s) in {1} milliseconds",
+                    buffer.Length, endTickCount - startTickCount);
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            SendAsync(input.Bytes);
+        }
+    }
+}
