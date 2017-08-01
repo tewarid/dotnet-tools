fe65e9745a1f4c6b9261351ef528418cba210cdd
Devendra Tewari <devendra.tewari@gmail.com>
2017-08-01 15:30:24 -0300

    HTTP Listener Tool

---
+++ HttpListenerTool/MainForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace HttpListenerTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.uri = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.directory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.stop = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.certificateAuth = new System.Windows.Forms.CheckBox();
+            this.log = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // uri
+            // 
+            this.uri.Location = new System.Drawing.Point(15, 191);
+            this.uri.Name = "uri";
+            this.uri.Size = new System.Drawing.Size(457, 20);
+            this.uri.TabIndex = 5;
+            this.uri.Text = "https://localhost:8088/";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Prefix URI";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(456, 146);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(292, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "For HTTPS, create and initialize X509 certificate as follows...";
+            // 
+            // directory
+            // 
+            this.directory.Location = new System.Drawing.Point(15, 231);
+            this.directory.Name = "directory";
+            this.directory.Size = new System.Drawing.Size(457, 20);
+            this.directory.TabIndex = 7;
+            this.directory.Text = "www";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 214);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Content Directory";
+            // 
+            // stop
+            // 
+            this.stop.Enabled = false;
+            this.stop.Location = new System.Drawing.Point(397, 466);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(75, 23);
+            this.stop.TabIndex = 24;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(316, 466);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 23;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // certificateAuth
+            // 
+            this.certificateAuth.AutoSize = true;
+            this.certificateAuth.Location = new System.Drawing.Point(15, 257);
+            this.certificateAuth.Name = "certificateAuth";
+            this.certificateAuth.Size = new System.Drawing.Size(145, 17);
+            this.certificateAuth.TabIndex = 9;
+            this.certificateAuth.Text = "Request Client Certificate";
+            this.certificateAuth.UseVisualStyleBackColor = true;
+            // 
+            // log
+            // 
+            this.log.Location = new System.Drawing.Point(15, 294);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.ReadOnly = true;
+            this.log.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.log.Size = new System.Drawing.Size(456, 163);
+            this.log.TabIndex = 11;
+            this.log.WordWrap = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Log";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 501);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.certificateAuth);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.directory);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uri);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "HTTP Listener Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox uri;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox directory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.CheckBox certificateAuth;
+        private System.Windows.Forms.TextBox log;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ HttpListenerTool/MainForm.cs
@@ -0,0 +1,151 @@
+﻿using MimeTypes;
+using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HttpListenerTool
+{
+    public partial class MainForm : Form
+    {
+        private HttpListener listener;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listener = new HttpListener();
+                listener.Prefixes.Add(uri.Text);
+                listener.Start();
+                start.Enabled = false;
+                stop.Enabled = true;
+                uri.ReadOnly = directory.ReadOnly = true;
+                while (listener.IsListening)
+                {
+                    await ListenAsync();
+                }
+            }
+            catch(Exception ex)
+            {
+                Log(ex.Message);
+            }
+        }
+
+        private async Task ListenAsync()
+        {
+            HttpListenerContext context = await listener.GetContextAsync();
+            HttpListenerRequest request = context.Request;
+            Log(string.Format("Received {0} request with URL {1}.", request.HttpMethod, 
+                request.Url.OriginalString));
+            if (certificateAuth.Checked)
+            {
+                X509Certificate2 certificate = await context.Request.GetClientCertificateAsync();
+                if (certificate == null)
+                {
+                    Log("Client certificate not received.");
+                    return;
+                }
+                Log(string.Format("Received client certificate with thumbprint {0}.", certificate.Thumbprint));
+            }
+            try
+            {
+                SendResponse(context);
+            }
+            catch(Exception ex)
+            {
+                Log(ex.Message);
+            }
+        }
+
+        private void SendResponse(HttpListenerContext context)
+        {
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+            string path = GetAbsoluteLocalPath(request.Url.LocalPath.Substring(1));
+            string file;
+            if (File.Exists(path))
+            {
+                file = path;
+            }
+            else
+            {
+                file = GetFirstFile(path);
+                if (string.IsNullOrEmpty(file))
+                {
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    response.Close();
+                    return;
+                }
+            }
+            string statusCodeString = Path.GetFileNameWithoutExtension(file);
+            int statusCode;
+            if (int.TryParse(statusCodeString, out statusCode))
+            {
+                response.StatusCode = statusCode;
+            }
+            switch (statusCode)
+            {
+                case 302:
+                    string [] location = File.ReadAllLines(file);
+                    response.AddHeader("Location", location.Length > 0 ? location[0] : string.Empty);
+                    break;
+                default:
+                    string mimeType = MimeTypeMap.GetMimeType(Path.GetExtension(file));
+                    response.ContentType = mimeType;
+                    if (request.HttpMethod != "HEAD")
+                    {
+                        byte[] data = File.ReadAllBytes(file);
+                        response.OutputStream.Write(data, 0, data.Length);
+                    }
+                    break;
+            }
+            response.Close();
+        }
+
+        private string GetFirstFile(string path)
+        {
+            string filename = null;
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path);
+                filename = files.Length > 0 ? files[0] : null;
+            }
+            return filename;
+        }
+
+        private string GetAbsoluteLocalPath(string localPath)
+        {
+            string path = string.Empty;
+            if (Path.IsPathRooted(directory.Text))
+            {
+                path = Path.Combine(directory.Text, localPath);
+            }
+            else
+            {
+                path = Path.Combine(Environment.CurrentDirectory, directory.Text, localPath);
+            }
+            return path;
+        }
+
+        private void Log(string text)
+        {
+            log.AppendText(text);
+            log.AppendText(Environment.NewLine);
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            listener.Stop();
+            start.Enabled = true;
+            stop.Enabled = false;
+            uri.ReadOnly = directory.ReadOnly = false;
+        }
+    }
+}
