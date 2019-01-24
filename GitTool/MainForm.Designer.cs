08a05d6eedf185cde2b1c2cf21762c4fd1351e34
Devendra Tewari <devendra.tewari@gmail.com>
2019-01-24 15:22:19 -0300

    add git tool

---
+++ GitTool/MainForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace GitTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.rootFolder = new System.Windows.Forms.TextBox();
+            this.browse = new System.Windows.Forms.Button();
+            this.scan = new System.Windows.Forms.Button();
+            this.gitFolders = new System.Windows.Forms.ListBox();
+            this.clipboard = new System.Windows.Forms.Button();
+            this.command = new System.Windows.Forms.TextBox();
+            this.run = new System.Windows.Forms.Button();
+            this.log = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rootFolder
+            // 
+            this.rootFolder.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rootFolder.Location = new System.Drawing.Point(13, 25);
+            this.rootFolder.Name = "rootFolder";
+            this.rootFolder.Size = new System.Drawing.Size(175, 20);
+            this.rootFolder.TabIndex = 0;
+            this.rootFolder.Text = "Z:\\git";
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(194, 23);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(75, 23);
+            this.browse.TabIndex = 1;
+            this.browse.Text = "Browse...";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // scan
+            // 
+            this.scan.Location = new System.Drawing.Point(274, 23);
+            this.scan.Name = "scan";
+            this.scan.Size = new System.Drawing.Size(75, 23);
+            this.scan.TabIndex = 3;
+            this.scan.Text = "Scan...";
+            this.scan.UseVisualStyleBackColor = true;
+            this.scan.Click += new System.EventHandler(this.scan_Click);
+            // 
+            // gitFolders
+            // 
+            this.gitFolders.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.gitFolders.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gitFolders.FormattingEnabled = true;
+            this.gitFolders.Location = new System.Drawing.Point(12, 68);
+            this.gitFolders.Name = "gitFolders";
+            this.gitFolders.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.gitFolders.Size = new System.Drawing.Size(337, 342);
+            this.gitFolders.TabIndex = 4;
+            // 
+            // clipboard
+            // 
+            this.clipboard.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.clipboard.Location = new System.Drawing.Point(224, 416);
+            this.clipboard.Name = "clipboard";
+            this.clipboard.Size = new System.Drawing.Size(125, 23);
+            this.clipboard.TabIndex = 5;
+            this.clipboard.Text = "Copy to Clipboard";
+            this.clipboard.UseVisualStyleBackColor = true;
+            this.clipboard.Click += new System.EventHandler(this.clipboard_Click);
+            // 
+            // command
+            // 
+            this.command.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.command.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.command.Location = new System.Drawing.Point(357, 22);
+            this.command.Multiline = true;
+            this.command.Name = "command";
+            this.command.Size = new System.Drawing.Size(431, 58);
+            this.command.TabIndex = 6;
+            this.command.Text = "config --list\r\nstatus";
+            // 
+            // run
+            // 
+            this.run.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.run.Location = new System.Drawing.Point(712, 86);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(75, 23);
+            this.run.TabIndex = 7;
+            this.run.Text = "Run...";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // log
+            // 
+            this.log.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.log.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.log.Location = new System.Drawing.Point(357, 115);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.ReadOnly = true;
+            this.log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.log.Size = new System.Drawing.Size(430, 293);
+            this.log.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Specify root Folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Select Git Repositories";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(357, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Run command(s) on selected repositories";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(357, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Output";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.run);
+            this.Controls.Add(this.command);
+            this.Controls.Add(this.clipboard);
+            this.Controls.Add(this.gitFolders);
+            this.Controls.Add(this.scan);
+            this.Controls.Add(this.browse);
+            this.Controls.Add(this.rootFolder);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Git Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowser;
+        private System.Windows.Forms.TextBox rootFolder;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.Button scan;
+        private System.Windows.Forms.ListBox gitFolders;
+        private System.Windows.Forms.Button clipboard;
+        private System.Windows.Forms.TextBox command;
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.TextBox log;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ GitTool/MainForm.cs
@@ -0,0 +1,134 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitTool
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void scan_Click(object sender, System.EventArgs e)
+        {
+            if (Directory.Exists(rootFolder.Text))
+            {
+                gitFolders.Items.Clear();
+                scan.Enabled = false;
+                Task.Run(() =>
+                {
+                    Scan(new DirectoryInfo(rootFolder.Text));
+                }).ContinueWith((task) => {
+                    BeginInvoke(new MethodInvoker(() =>
+                    {
+                        scan.Enabled = true;
+                    }));
+                });
+            }
+            else
+            {
+                MessageBox.Show(this, $"Folder {rootFolder.Text} not found.");
+            }
+        }
+
+        private void Scan(DirectoryInfo rootDir)
+        {
+            if (rootDir.Name.Equals(".git"))
+            {
+                return;
+            }
+            else if (Repository.IsValid(rootDir.FullName))
+            {
+                BeginInvoke(new MethodInvoker(() =>
+                {
+                    gitFolders.Items.Add(rootDir.FullName);
+                }));
+                return;
+            }
+            DirectoryInfo[] subDirs = rootDir.GetDirectories();
+            foreach (DirectoryInfo dir in subDirs)
+            {
+                 Scan(dir);
+            }
+        }
+
+        private IList GetFolders()
+        {
+            IList list = gitFolders.Items;
+            if (gitFolders.SelectedItems.Count > 0)
+            {
+                list = gitFolders.SelectedItems;
+            }
+            return list;
+        }
+
+        private void clipboard_Click(object sender, EventArgs e)
+        {
+            IList list = GetFolders();
+            string[] folders = new string[list.Count];
+            list.CopyTo(folders, 0);
+            Clipboard.SetText(String.Join(Environment.NewLine, folders));
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            folderBrowser.SelectedPath = rootFolder.Text;
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                rootFolder.Text = folderBrowser.SelectedPath;
+            }
+        }
+
+        private void run_Click(object sender, EventArgs e)
+        {
+            log.Clear();
+            string [] gitCommands = command.Text.Split(new string[] { Environment.NewLine },
+                StringSplitOptions.RemoveEmptyEntries);
+            foreach (string folder in GetFolders())
+            {
+                foreach(string cmd in gitCommands)
+                {
+                    RunGitCommand(folder, cmd);
+                }
+            }
+        }
+
+        private void RunGitCommand(string gitFolder, string command)
+        {
+            log.AppendText($"{gitFolder}> git {command}{Environment.NewLine}");
+            ProcessStartInfo info = new ProcessStartInfo
+            {
+                FileName = "git",
+                Arguments = command,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true,
+                WorkingDirectory = gitFolder
+
+            };
+            Process proc = new Process();
+            proc.StartInfo = info;
+            proc.OutputDataReceived += new DataReceivedEventHandler((dataSender, dataEvent) =>
+            {
+                log.AppendText(dataEvent.Data);
+            });
+            proc.Start();
+            proc.WaitForExit();
+            log.AppendText(ReplaceNewLine(proc.StandardError.ReadToEnd()));
+            log.AppendText(ReplaceNewLine(proc.StandardOutput.ReadToEnd()));
+            log.AppendText(Environment.NewLine);
+        }
+
+        private static string ReplaceNewLine(string old)
+        {
+            return old.Replace("\n", Environment.NewLine);
+        }
+    }
+}
