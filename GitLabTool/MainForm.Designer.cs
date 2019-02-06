72bc68e00de2aa6b326d2b7814e94113d50711a1
Devendra Tewari <devendra.tewari@gmail.com>
2019-02-06 14:05:53 -0300

    gitlab tool

---
+++ GitLabTool/MainForm.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace GitLabTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.password = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.projects = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.milestones = new System.Windows.Forms.ListView();
+            this.query = new System.Windows.Forms.Button();
+            this.username = new System.Windows.Forms.TextBox();
+            this.host = new System.Windows.Forms.TextBox();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(12, 112);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(176, 20);
+            this.password.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Password or access token";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Username";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Host";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(203, 7);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(589, 440);
+            this.tabControl1.TabIndex = 24;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.projects);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(581, 414);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Projects";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // projects
+            // 
+            this.projects.AllowColumnReorder = true;
+            this.projects.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.projects.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.projects.FullRowSelect = true;
+            this.projects.Location = new System.Drawing.Point(3, 3);
+            this.projects.Name = "projects";
+            this.projects.Size = new System.Drawing.Size(575, 408);
+            this.projects.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.projects.TabIndex = 0;
+            this.projects.UseCompatibleStateImageBehavior = false;
+            this.projects.View = System.Windows.Forms.View.Details;
+            this.projects.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDown);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 250;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "HTTP URL";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "SSH URL";
+            this.columnHeader3.Width = 100;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.milestones);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(581, 414);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Milestones";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // milestones
+            // 
+            this.milestones.AllowColumnReorder = true;
+            this.milestones.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.milestones.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.milestones.FullRowSelect = true;
+            this.milestones.Location = new System.Drawing.Point(3, 3);
+            this.milestones.Name = "milestones";
+            this.milestones.Size = new System.Drawing.Size(575, 408);
+            this.milestones.Sorting = System.Windows.Forms.SortOrder.Descending;
+            this.milestones.TabIndex = 2;
+            this.milestones.UseCompatibleStateImageBehavior = false;
+            this.milestones.View = System.Windows.Forms.View.Details;
+            this.milestones.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDown);
+            // 
+            // query
+            // 
+            this.query.Location = new System.Drawing.Point(113, 138);
+            this.query.Name = "query";
+            this.query.Size = new System.Drawing.Size(75, 23);
+            this.query.TabIndex = 1;
+            this.query.Text = "Query";
+            this.query.UseVisualStyleBackColor = true;
+            this.query.Click += new System.EventHandler(this.query_Click);
+            // 
+            // username
+            // 
+            this.username.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::GitLabTool.Properties.Settings.Default, "username", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.username.Location = new System.Drawing.Point(12, 68);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(176, 20);
+            this.username.TabIndex = 21;
+            this.username.Text = global::GitLabTool.Properties.Settings.Default.username;
+            // 
+            // host
+            // 
+            this.host.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::GitLabTool.Properties.Settings.Default, "host", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.host.Location = new System.Drawing.Point(12, 24);
+            this.host.Name = "host";
+            this.host.Size = new System.Drawing.Size(176, 20);
+            this.host.TabIndex = 19;
+            this.host.Text = global::GitLabTool.Properties.Settings.Default.host;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Due Date";
+            this.columnHeader4.Width = 90;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Project";
+            this.columnHeader5.Width = 160;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Title";
+            this.columnHeader6.Width = 170;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "State";
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Start Date";
+            this.columnHeader8.Width = 90;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 450);
+            this.Controls.Add(this.query);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.host);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "GitLab Tool";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox host;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button query;
+        private System.Windows.Forms.ListView projects;
+        private System.Windows.Forms.ListView milestones;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+    }
+}
+
---
+++ GitLabTool/MainForm.cs
@@ -0,0 +1,104 @@
+﻿using GitLabApiClient;
+using GitLabApiClient.Models.Milestones.Responses;
+using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitLabTool
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void query_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await Query().ConfigureAwait(true);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(this, ex.Message);
+            }
+        }
+
+        private async Task Query()
+        {
+            GitLabClient client;
+            if (string.IsNullOrEmpty(username.Text))
+            {
+                client = new GitLabClient(host.Text, password.Text);
+            }
+            else
+            {
+                client = new GitLabClient(host.Text);
+                var session = await client.LoginAsync(username.Text, password.Text);
+            }
+            var projectList = await client.Projects.GetAsync().ConfigureAwait(true);
+            projects.Items.Clear();
+            milestones.Items.Clear();
+            foreach (var project in projectList)
+            {
+                projects.Items.Add(new ListViewItem(new string[] {
+                    project.PathWithNamespace,
+                    project.HttpUrlToRepo,
+                    project.SshUrlToRepo }));
+                try
+                {
+                    var milestoneList = await client.Projects
+                        .GetMilestonesAsync(project.Id, (options) =>
+                        {
+                            options.State = MilestoneState.All;
+                        }).ConfigureAwait(true);
+                    foreach (var milestone in milestoneList)
+                    {
+                        milestones.Items.Add(new ListViewItem(new string[] {
+                            milestone.DueDate,
+                            project.PathWithNamespace,
+                            milestone.Title,
+                            milestone.State.ToString(),
+                            milestone.StartDate}));
+                    }
+                }
+                catch (GitLabException ex)
+                {
+                    if (ex.HttpStatusCode == HttpStatusCode.Forbidden)
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+
+        private void KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                CopyListBox((ListView)sender);
+            }
+        }
+
+        public void CopyListBox(ListView list)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in list.SelectedItems)
+            {
+                ListViewItem l = item as ListViewItem;
+                if (l != null)
+                {
+                    foreach(ListViewItem.ListViewSubItem sub in l.SubItems)
+                    {
+                        sb.Append(sub.Text + "\t");
+                    }
+                }
+                sb.AppendLine();
+            }
+            Clipboard.SetDataObject(sb.ToString());
+        }
+    }
+}
