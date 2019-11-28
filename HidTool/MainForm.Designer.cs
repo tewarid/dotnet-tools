8be4f6b842df9f7353f4bdd7b02e70bb5b8eeafc
Devendra Tewari <devendra.tewari@gmail.com>
2019-11-28 15:49:26 -0300

    send and receive raw reports

--- HidTool/MainForm.Designer.cs
+++ HidTool/MainForm.Designer.cs
@@ -31,20 +31,40 @@
             this.hidDevicesCombo = new System.Windows.Forms.ComboBox();
             this.refresh = new System.Windows.Forms.Button();
             this.open = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.send = new System.Windows.Forms.Button();
+            this.input = new Common.InputTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.outputText = new Common.OutputTextBox();
+            this.reuseAddress = new System.Windows.Forms.CheckBox();
+            this.sourcePort = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.status = new System.Windows.Forms.ToolStripStatusLabel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // hidDevicesCombo
             // 
             this.hidDevicesCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.hidDevicesCombo.FormattingEnabled = true;
-            this.hidDevicesCombo.Location = new System.Drawing.Point(13, 13);
+            this.hidDevicesCombo.Location = new System.Drawing.Point(12, 12);
             this.hidDevicesCombo.Name = "hidDevicesCombo";
             this.hidDevicesCombo.Size = new System.Drawing.Size(430, 21);
             this.hidDevicesCombo.TabIndex = 0;
+            this.hidDevicesCombo.SelectedIndexChanged += new System.EventHandler(this.hidDevicesCombo_SelectedIndexChanged);
             // 
             // refresh
             // 
-            this.refresh.Location = new System.Drawing.Point(449, 13);
+            this.refresh.Location = new System.Drawing.Point(448, 12);
             this.refresh.Name = "refresh";
             this.refresh.Size = new System.Drawing.Size(75, 23);
             this.refresh.TabIndex = 1;
@@ -54,7 +74,7 @@
             // 
             // open
             // 
-            this.open.Location = new System.Drawing.Point(531, 13);
+            this.open.Location = new System.Drawing.Point(530, 12);
             this.open.Name = "open";
             this.open.Size = new System.Drawing.Size(75, 23);
             this.open.TabIndex = 2;
@@ -62,18 +82,161 @@
             this.open.UseVisualStyleBackColor = true;
             this.open.Click += new System.EventHandler(this.Open_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.send);
+            this.panel1.Controls.Add(this.input);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(398, 386);
+            this.panel1.TabIndex = 0;
+            // 
+            // send
+            // 
+            this.send.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.send.Location = new System.Drawing.Point(320, 360);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(75, 23);
+            this.send.TabIndex = 27;
+            this.send.Text = "Send";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // input
+            // 
+            this.input.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.input.BinaryChecked = true;
+            this.input.ChangeEndOfLine = true;
+            this.input.EndOfLine = Common.EndOfLine.Dos;
+            this.input.Location = new System.Drawing.Point(7, 9);
+            this.input.MinimumSize = new System.Drawing.Size(280, 130);
+            this.input.Name = "input";
+            this.input.SelectedTextValue = "";
+            this.input.Size = new System.Drawing.Size(388, 374);
+            this.input.TabIndex = 29;
+            this.input.TextValue = "";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.outputText);
+            this.panel2.Controls.Add(this.reuseAddress);
+            this.panel2.Controls.Add(this.sourcePort);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(398, 386);
+            this.panel2.TabIndex = 1;
+            // 
+            // outputText
+            // 
+            this.outputText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputText.AppendBinaryChecked = true;
+            this.outputText.Location = new System.Drawing.Point(8, 9);
+            this.outputText.MinimumSize = new System.Drawing.Size(355, 95);
+            this.outputText.Name = "outputText";
+            this.outputText.Size = new System.Drawing.Size(382, 374);
+            this.outputText.TabIndex = 11;
+            this.outputText.TextValue = "";
+            // 
+            // reuseAddress
+            // 
+            this.reuseAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.reuseAddress.AutoSize = true;
+            this.reuseAddress.Location = new System.Drawing.Point(611, 25);
+            this.reuseAddress.Name = "reuseAddress";
+            this.reuseAddress.Size = new System.Drawing.Size(98, 17);
+            this.reuseAddress.TabIndex = 3;
+            this.reuseAddress.Text = "Reuse Address";
+            this.reuseAddress.UseVisualStyleBackColor = true;
+            // 
+            // sourcePort
+            // 
+            this.sourcePort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.sourcePort.Location = new System.Drawing.Point(511, 23);
+            this.sourcePort.Name = "sourcePort";
+            this.sourcePort.Size = new System.Drawing.Size(94, 20);
+            this.sourcePort.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(508, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Source Port";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.status});
+            this.statusStrip.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip.TabIndex = 15;
+            // 
+            // status
+            // 
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 17);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 39);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 386);
+            this.splitContainer1.SplitterDistance = 398;
+            this.splitContainer1.TabIndex = 16;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "pfx";
+            this.openFileDialog.Filter = "PFX files|*.pfx";
+            this.openFileDialog.Title = "Pick PFX file";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.open);
             this.Controls.Add(this.refresh);
             this.Controls.Add(this.hidDevicesCombo);
             this.Name = "MainForm";
             this.Text = "HID Tool";
             this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,6 +245,18 @@
         private System.Windows.Forms.ComboBox hidDevicesCombo;
         private System.Windows.Forms.Button refresh;
         private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.Panel panel2;
+        private Common.OutputTextBox outputText;
+        private System.Windows.Forms.CheckBox reuseAddress;
+        private System.Windows.Forms.TextBox sourcePort;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel status;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private Common.InputTextBox input;
     }
 }
 
--- HidTool/MainForm.cs
+++ HidTool/MainForm.cs
@@ -1,6 +1,11 @@
 ﻿using HidSharp;
+using HidSharp.Reports;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HidTool
@@ -8,7 +13,7 @@ namespace HidTool
     public partial class MainForm : Form
     {
         List<HidDevice> hidDevices = new List<HidDevice>();
-        HidStream stream;
+        HidDevice device;
 
         public MainForm()
         {
@@ -57,10 +62,161 @@ namespace HidTool
                 MessageBox.Show(this, "Please select a HID device in the list.");
                 return;
             }
-            if (!hidDevices[hidDevicesCombo.SelectedIndex].TryOpen(out stream))
+            device = hidDevices[hidDevicesCombo.SelectedIndex];
+            Task.Run(() =>
             {
-                MessageBox.Show(this, "Could not open HID stream. Device may be in use.");
+                Read(device);
+            });
+        }
+
+        private void Read(HidDevice device)
+        {
+            if (device == null)
+            {
+                return;
             }
+            ReportDescriptor reportDescriptor;
+            try
+            {
+                reportDescriptor = device.GetReportDescriptor();
+            }
+            catch (Exception ex)
+            {
+                BeginInvoke(new MethodInvoker(() =>
+                {
+                    outputText.AppendText($"Read failed: {ex.Message}{Environment.NewLine}");
+                }));
+                return;
+            }
+            HidStream hidStream;
+            if (!device.TryOpen(out hidStream))
+            {
+                BeginInvoke(new MethodInvoker(() =>
+                {
+                    outputText.AppendText($"Could not open stream.{Environment.NewLine}");
+                }));
+                return;
+            }
+            var inputReportBuffer = new byte[device.GetMaxInputReportLength()];
+            var inputReceiver = reportDescriptor.CreateHidDeviceInputReceiver();
+            inputReceiver.Start(hidStream);
+            while (true)
+            {
+                if (!inputReceiver.WaitHandle.WaitOne(1000))
+                {
+                    continue; // timeout
+                }
+                if (!inputReceiver.IsRunning)
+                {
+                    break;
+                } // Disconnected?
+                Report report;
+                while (inputReceiver.TryRead(inputReportBuffer, 0, out report))
+                {
+                    BeginInvoke(new MethodInvoker(() =>
+                    {
+                        ShowReceivedData(inputReportBuffer);
+                    }));
+                }
+            }
+            hidStream.Dispose();
+            BeginInvoke(new MethodInvoker(() =>
+            {
+                outputText.AppendText($"Disconnected.{Environment.NewLine}");
+            }));
+        }
+        private void ShowReceivedData(byte[] data)
+        {
+            outputText.AppendText($"{data.Length} bytes(s) received on {DateTime.Now}:{Environment.NewLine}");
+            outputText.AppendBinary(data, data.Length);
+            outputText.AppendText(Environment.NewLine);
+            outputText.AppendText(Environment.NewLine);
+        }
+
+        private async void Send_Click(object sender, EventArgs e)
+        {
+            if (device == null)
+            {
+                return;
+            }
+
+            send.Enabled = false;
+
+            byte[] data = input.BinaryValue;
+            if (data.Length <= 0)
+            {
+                send.Enabled = true;
+                MessageBox.Show(this, "Nothing to send.", this.Text);
+                return;
+            }
+
+            HidStream hidStream;
+            if (!device.TryOpen(out hidStream))
+            {
+                return;
+            }
+
+            int tickcount = Environment.TickCount;
+            try
+            {
+                await hidStream.WriteAsync(data, 0, data.Length).ConfigureAwait(true);
+            }
+            catch (IOException ex)
+            {
+                send.Enabled = true;
+                MessageBox.Show(this, ex.Message, this.Text);
+                return;
+            }
+            finally
+            {
+                hidStream.Dispose();
+            }
+            tickcount = Environment.TickCount - tickcount;
+
+            status.Text = String.Format("Sent {0} byte(s) in {1} milliseconds",
+                data.Length, tickcount);
+            send.Enabled = true;
+        }
+
+        private void hidDevicesCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            HidDevice device = hidDevices[hidDevicesCombo.SelectedIndex];
+            outputText.AppendText(string.Format("Selected {0}, Serial Number {1}, Vendor ID 0x{2:X}, Product ID 0x{3:X}{4}",
+                device.GetFriendlyName(), device.GetSerialNumber(), device.VendorID, device.ProductID, Environment.NewLine));
+            ReportDescriptor reportDescriptor;
+            try
+            {
+                reportDescriptor = device.GetReportDescriptor();
+            }
+            catch (Exception ex)
+            {
+                BeginInvoke(new MethodInvoker(() =>
+                {
+                    outputText.AppendText($"Could not get report descriptor: {ex.Message}{Environment.NewLine}");
+                }));
+                return;
+            }
+            foreach (var deviceItem in reportDescriptor.DeviceItems)
+            {
+                foreach (var usage in deviceItem.Usages.GetAllValues())
+                {
+                    outputText.AppendText($"Usage: {usage:X4} {(Usage)usage}{Environment.NewLine}");
+                }
+                foreach (var report in deviceItem.Reports)
+                {
+                    outputText.AppendText(string.Format("{0}: ReportID={1}, Length={2}, Items={3}{4}",
+                                        report.ReportType, report.ReportID, report.Length,
+                                        report.DataItems.Count, Environment.NewLine));
+                    foreach (var dataItem in report.DataItems)
+                    {
+                        outputText.AppendText(string.Format("  {0} Elements x {1} Bits, Units: {2}, Expected Usage Type: {3}, Flags: {4}, Usages: {5}{6}",
+                            dataItem.ElementCount, dataItem.ElementBits, dataItem.Unit.System, dataItem.ExpectedUsageType, dataItem.Flags,
+                            string.Join(", ", dataItem.Usages.GetAllValues().Select(usage => usage.ToString("X4") + " " + ((Usage)usage).ToString())),
+                            Environment.NewLine));
+                    }
+                }
+            }
+            outputText.AppendText(Environment.NewLine);
         }
     }
 }
