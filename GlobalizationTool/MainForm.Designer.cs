498feb32e3eaefbc28f82b3955113f19d3363f61
Devendra Tewari <devendra.tewari@gmail.com>
2019-12-26 15:58:20 -0300

    globalization tool

---
+++ GlobalizationTool/MainForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace GlobalizationTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.culturesGrid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.culturesGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // culturesGrid
+            // 
+            this.culturesGrid.AllowUserToAddRows = false;
+            this.culturesGrid.AllowUserToDeleteRows = false;
+            this.culturesGrid.AllowUserToResizeRows = false;
+            this.culturesGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.culturesGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.culturesGrid.DefaultCellStyle = dataGridViewCellStyle1;
+            this.culturesGrid.Location = new System.Drawing.Point(3, 26);
+            this.culturesGrid.Name = "culturesGrid";
+            this.culturesGrid.ReadOnly = true;
+            this.culturesGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.culturesGrid.Size = new System.Drawing.Size(777, 523);
+            this.culturesGrid.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cultures";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.culturesGrid);
+            this.Name = "MainForm";
+            this.Text = "Globalization Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.culturesGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView culturesGrid;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GlobalizationTool/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace GlobalizationTool
+{
+    public partial class MainForm : Form
+    {
+        List<CultureInfo> cultures = new List<CultureInfo>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            foreach (CultureInfo ci in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            {
+                string specName = "(none)";
+                try { specName = CultureInfo.CreateSpecificCulture(ci.Name).Name; }
+
+                catch { }
+                cultures.Add(ci);
+            }
+            var data = cultures
+                .Select(item => new
+                {
+                    item.EnglishName,
+                    item.Name,
+                    SpecificName = CultureInfo.CreateSpecificCulture(item.Name).Name,
+                    item.DateTimeFormat.ShortDatePattern,
+                    item.NumberFormat.NumberDecimalSeparator,
+                    item.TwoLetterISOLanguageName,
+                    item.ThreeLetterISOLanguageName
+                })
+                .OrderBy(item => item.EnglishName);
+            BindingSource bs = new BindingSource();
+            bs.DataSource = data;
+            culturesGrid.DataSource = bs;
+        }
+    }
+}
