ad5e58511dfb4e55fcfa825bffb655737a9b0ac8
Devendra Tewari <devendra.tewari@gmail.com>
2020-02-04 14:46:59 -0300

    unicode character grid

--- GlobalizationTool/MainForm.Designer.cs
+++ GlobalizationTool/MainForm.Designer.cs
@@ -28,11 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.tab = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
             this.culturesGrid = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.unicodeGrid = new System.Windows.Forms.DataGridView();
+            this.tab.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.culturesGrid)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.unicodeGrid)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tab
+            // 
+            this.tab.Controls.Add(this.tabPage1);
+            this.tab.Controls.Add(this.tabPage2);
+            this.tab.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tab.Location = new System.Drawing.Point(0, 0);
+            this.tab.Name = "tab";
+            this.tab.SelectedIndex = 0;
+            this.tab.Size = new System.Drawing.Size(784, 561);
+            this.tab.TabIndex = 2;
+            this.tab.SelectedIndexChanged += new System.EventHandler(this.Tab_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.culturesGrid);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(776, 535);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Cultures";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // culturesGrid
             // 
@@ -43,50 +73,73 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.culturesGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.culturesGrid.DefaultCellStyle = dataGridViewCellStyle1;
-            this.culturesGrid.Location = new System.Drawing.Point(3, 26);
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.culturesGrid.DefaultCellStyle = dataGridViewCellStyle4;
+            this.culturesGrid.Location = new System.Drawing.Point(3, 3);
             this.culturesGrid.Name = "culturesGrid";
             this.culturesGrid.ReadOnly = true;
             this.culturesGrid.RowHeadersVisible = false;
             this.culturesGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.culturesGrid.Size = new System.Drawing.Size(777, 523);
-            this.culturesGrid.TabIndex = 0;
+            this.culturesGrid.Size = new System.Drawing.Size(770, 529);
+            this.culturesGrid.TabIndex = 2;
             // 
-            // label1
+            // tabPage2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(0, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Cultures";
+            this.tabPage2.Controls.Add(this.unicodeGrid);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(776, 535);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Unicode";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // unicodeGrid
+            // 
+            this.unicodeGrid.AllowUserToAddRows = false;
+            this.unicodeGrid.AllowUserToDeleteRows = false;
+            this.unicodeGrid.AllowUserToResizeRows = false;
+            this.unicodeGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.unicodeGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.unicodeGrid.Location = new System.Drawing.Point(4, 4);
+            this.unicodeGrid.Name = "unicodeGrid";
+            this.unicodeGrid.ReadOnly = true;
+            this.unicodeGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.unicodeGrid.Size = new System.Drawing.Size(769, 528);
+            this.unicodeGrid.TabIndex = 0;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.culturesGrid);
+            this.Controls.Add(this.tab);
             this.Name = "MainForm";
             this.Text = "Globalization Tool";
+            this.tab.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.culturesGrid)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.unicodeGrid)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TabControl tab;
+        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.DataGridView culturesGrid;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView unicodeGrid;
     }
 }
 
--- GlobalizationTool/MainForm.cs
+++ GlobalizationTool/MainForm.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using CsvHelper;
 using System.Globalization;
-using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
@@ -8,35 +9,75 @@ namespace GlobalizationTool
 {
     public partial class MainForm : Form
     {
-        List<CultureInfo> cultures = new List<CultureInfo>();
-
         public MainForm()
         {
             InitializeComponent();
+
+            culturesGrid.Columns.Clear();
+            culturesGrid.Columns.Add("EnglishName", "English Name");
+            culturesGrid.Columns.Add("Name", "Name");
+            culturesGrid.Columns.Add("SpecificName", "Specific Name");
+            culturesGrid.Columns.Add("LCID", "LCID");
+            culturesGrid.Columns.Add("ShortDatePattern", "Short Date Pattern");
+            culturesGrid.Columns.Add("DecimalSeparator", "Decimal Separator");
+            culturesGrid.Columns.Add("TwoLetterISOLanguageName", "ISO Name");
             foreach (CultureInfo ci in CultureInfo.GetCultures(CultureTypes.AllCultures))
             {
                 string specName = "(none)";
                 try { specName = CultureInfo.CreateSpecificCulture(ci.Name).Name; }
 
                 catch { }
-                cultures.Add(ci);
-            }
-            var data = cultures
-                .Select(item => new
+                //cultures.Add(ci);
+                culturesGrid.Rows.Add(new object[]
                 {
-                    item.EnglishName,
-                    item.Name,
-                    SpecificName = CultureInfo.CreateSpecificCulture(item.Name).Name,
-                    item.LCID,
-                    item.DateTimeFormat.ShortDatePattern,
-                    item.NumberFormat.NumberDecimalSeparator,
-                    item.TwoLetterISOLanguageName,
-                    item.ThreeLetterISOLanguageName
-                })
-                .OrderBy(item => item.EnglishName);
-            BindingSource bs = new BindingSource();
-            bs.DataSource = data;
-            culturesGrid.DataSource = bs;
+                    ci.EnglishName,
+                    ci.Name,
+                    CultureInfo.CreateSpecificCulture(ci.Name).Name,
+                    ci.LCID,
+                    ci. DateTimeFormat.ShortDatePattern,
+                    ci.NumberFormat.NumberDecimalSeparator,
+                    ci.TwoLetterISOLanguageName
+                });
+            }
+        }
+
+        private void Tab_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            if (tab.SelectedIndex == 1)
+            {
+                LoadUnicodeGrid();
+            }
+        }
+
+        private void LoadUnicodeGrid()
+        {
+            if (unicodeGrid.Rows.Count != 0)
+            {
+                return;
+            }
+            unicodeGrid.Columns.Clear();
+            unicodeGrid.Columns.Add("Code", "Code (hex)");
+            unicodeGrid.Columns.Add("Code", "Code (dec)");
+            unicodeGrid.Columns.Add("Name", "Name");
+            unicodeGrid.Columns.Add("Display", "Display");
+            using (var reader = new StreamReader("UnicodeData.txt"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Configuration.HasHeaderRecord = false;
+                csv.Configuration.Delimiter = ";";
+                var records = csv.GetRecords<dynamic>();
+                foreach (var record in records)
+                {
+                    int value = int.Parse(record.Field1, NumberStyles.HexNumber);
+                    unicodeGrid.Rows.Add(new object[]
+                    {
+                        record.Field1,
+                        value,
+                        record.Field2,
+                        (char)value
+                    });
+                }
+            }
         }
     }
 }
