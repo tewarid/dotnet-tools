2ede5fead9f3e8fbf69961ce86fd5b2056285204
Devendra Tewari <devendra.tewari@gmail.com>
2020-09-15 13:20:49 -0300

    mdns service discovery tool

---
+++ ServiceDiscoveryTool/MainForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿using System.Windows.Forms;
+
+namespace ServiceBrowserTool
+{
+    partial class MainForm : Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.domainText = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serviceType = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.networkProtocol = new System.Windows.Forms.ComboBox();
+            this.startWatcher = new System.Windows.Forms.Button();
+            this.stopWatcher = new System.Windows.Forms.Button();
+            this.output = new Common.OutputTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Domain";
+            // 
+            // domainText
+            // 
+            this.domainText.Items.AddRange(new object[] {
+            "local",
+            "apple.com"});
+            this.domainText.Location = new System.Drawing.Point(12, 28);
+            this.domainText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.domainText.Name = "domainText";
+            this.domainText.Size = new System.Drawing.Size(118, 21);
+            this.domainText.TabIndex = 1;
+            this.domainText.Text = "local";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 50);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "DNS-SD Service Type";
+            // 
+            // serviceType
+            // 
+            this.serviceType.Items.AddRange(new object[] {
+            "_airplay",
+            "_daap",
+            "_googlecast",
+            "_homekit",
+            "_mediaremotetv",
+            "_raop"});
+            this.serviceType.Location = new System.Drawing.Point(12, 66);
+            this.serviceType.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.serviceType.Name = "serviceType";
+            this.serviceType.Size = new System.Drawing.Size(118, 21);
+            this.serviceType.TabIndex = 3;
+            this.serviceType.Text = "_googlecast";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 88);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Network Protocol";
+            // 
+            // networkProtocol
+            // 
+            this.networkProtocol.Items.AddRange(new object[] {
+            "_tcp"});
+            this.networkProtocol.Location = new System.Drawing.Point(12, 104);
+            this.networkProtocol.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.networkProtocol.Name = "networkProtocol";
+            this.networkProtocol.Size = new System.Drawing.Size(118, 21);
+            this.networkProtocol.TabIndex = 5;
+            this.networkProtocol.Text = "_tcp";
+            // 
+            // startWatcher
+            // 
+            this.startWatcher.Location = new System.Drawing.Point(12, 136);
+            this.startWatcher.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.startWatcher.Name = "startWatcher";
+            this.startWatcher.Size = new System.Drawing.Size(56, 19);
+            this.startWatcher.TabIndex = 6;
+            this.startWatcher.Text = "Start";
+            this.startWatcher.UseVisualStyleBackColor = true;
+            this.startWatcher.Click += new System.EventHandler(this.StartWatcher_Click);
+            // 
+            // stopWatcher
+            // 
+            this.stopWatcher.Enabled = false;
+            this.stopWatcher.Location = new System.Drawing.Point(73, 136);
+            this.stopWatcher.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.stopWatcher.Name = "stopWatcher";
+            this.stopWatcher.Size = new System.Drawing.Size(56, 19);
+            this.stopWatcher.TabIndex = 7;
+            this.stopWatcher.Text = "Stop";
+            this.stopWatcher.UseVisualStyleBackColor = true;
+            this.stopWatcher.Click += new System.EventHandler(this.StopWatcher_Click);
+            // 
+            // output
+            // 
+            this.output.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.output.AppendBinaryChecked = false;
+            this.output.Location = new System.Drawing.Point(148, 11);
+            this.output.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(442, 345);
+            this.output.TabIndex = 8;
+            this.output.TextValue = "";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 171);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 96);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Use Wireshark to view mDNS/DNS-SD queries sent to devices and their reponses. Use" +
+    " protocol filter mdns.";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.stopWatcher);
+            this.Controls.Add(this.startWatcher);
+            this.Controls.Add(this.networkProtocol);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.serviceType);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.domainText);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MainForm";
+            this.Text = "Service Discovery Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox domainText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox serviceType;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox networkProtocol;
+        private System.Windows.Forms.Button startWatcher;
+        private System.Windows.Forms.Button stopWatcher;
+        private Common.OutputTextBox output;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ServiceDiscoveryTool/MainForm.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.Devices.Enumeration;
+
+namespace ServiceBrowserTool
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// The protocol ID that identifies DNS-SD.
+        /// </summary>
+        private const string PROTOCOL_GUID = "{4526e8c1-8aac-4153-9b16-55e86ada0e54}";
+
+        /// <summary>
+        /// The host name property.
+        /// </summary>
+        private const string HOSTNAME_PROPERTY = "System.Devices.Dnssd.HostName";
+
+        /// <summary>
+        /// The service name property.
+        /// </summary>
+        private const string SERVICENAME_PROPERTY = "System.Devices.Dnssd.ServiceName";
+
+        /// <summary>
+        /// The instance name property.
+        /// </summary>
+        private const string INSTANCENAME_PROPERTY = "System.Devices.Dnssd.InstanceName";
+
+        /// <summary>
+        /// The IP address property.
+        /// </summary>
+        private const string IPADDRESS_PROPERTY = "System.Devices.IpAddress";
+
+        /// <summary>
+        /// The port number property.
+        /// </summary>
+        private const string PORTNUMBER_PROPERTY = "System.Devices.Dnssd.PortNumber";
+
+        /// <summary>
+        /// All of the properties that will be returned when a DNS-SD instance has been found. 
+        /// </summary>
+        private readonly string[] _propertyKeys = new string[] {
+            HOSTNAME_PROPERTY,
+            SERVICENAME_PROPERTY,
+            INSTANCENAME_PROPERTY,
+            IPADDRESS_PROPERTY,
+            PORTNUMBER_PROPERTY
+        };
+
+        /// <summary>
+        /// The device watcher that will be watching for DNS-SD connections.
+        /// </summary>
+        private DeviceWatcher _watcher;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation args)
+        {
+            BeginInvoke((Action)(() => {
+                output.AppendText($"Device Added{Environment.NewLine}");
+                output.AppendText($"\tKind: {args.Kind}{Environment.NewLine}");
+                output.AppendText($"\tId: {args.Id}{Environment.NewLine}");
+                output.AppendText($"\tName: {args.Name}{Environment.NewLine}");
+                output.AppendText($"\tIP Address: {(args.Properties[IPADDRESS_PROPERTY] as string[])[0]}{Environment.NewLine}");
+                output.AppendText($"\tPort: {args.Properties[PORTNUMBER_PROPERTY]}{Environment.NewLine}");
+                output.AppendText($"{Environment.NewLine}");
+            }));
+            await Task.CompletedTask.ConfigureAwait(false);
+        }
+
+        private async void DeviceWatcher_Updated(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            BeginInvoke((Action)(() => {
+                output.AppendText($"Device Updated{Environment.NewLine}");
+                output.AppendText($"\tKind: {args.Kind}{Environment.NewLine}");
+                output.AppendText($"\tId: {args.Id}{Environment.NewLine}");
+                output.AppendText($"\tIP Address: {(args.Properties[IPADDRESS_PROPERTY] as string[])[0]}{Environment.NewLine}");
+                output.AppendText($"\tPort: {args.Properties[PORTNUMBER_PROPERTY]}{Environment.NewLine}");
+                output.AppendText($"{Environment.NewLine}");
+            }));
+            await Task.CompletedTask.ConfigureAwait(false);
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Event handler")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>")]
+        private void StartWatcher_Click(object sender, EventArgs e)
+        {
+            string _aqsQueryString = $"System.Devices.AepService.ProtocolId:={PROTOCOL_GUID}"
+                + $" AND System.Devices.Dnssd.Domain:=\"{domainText.Text}\""
+                + $" AND System.Devices.Dnssd.ServiceName:=\"{serviceType.Text}.{networkProtocol.Text}\"";
+
+            _watcher = DeviceInformation.CreateWatcher(_aqsQueryString, _propertyKeys, DeviceInformationKind.AssociationEndpointService);
+            _watcher.Added += DeviceWatcher_Added;
+            _watcher.Updated += DeviceWatcher_Updated;
+            _watcher.Start();
+            EnableDisable(false);
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Event handler")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>")]
+        private void StopWatcher_Click(object sender, EventArgs e)
+        {
+            _watcher.Stop();
+            _watcher = null;
+            EnableDisable(true);
+        }
+
+        private void EnableDisable(bool enable)
+        {
+            startWatcher.Enabled = enable;
+            domainText.Enabled = enable;
+            serviceType.Enabled = enable;
+            networkProtocol.Enabled = enable;
+            stopWatcher.Enabled = !enable;
+        }
+    }
+}
